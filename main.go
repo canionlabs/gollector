4cdfd209b37dd12ad971d9ffebef4c243e6cd5d3
caiovictormc <caiovictormc@gmail.com>
2019-03-05 20:07:18 -0300

    CREATED: main initial structure

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	amqpURL := flag.String("amqpUrl", "amqp://guest:guest@localhost:5672/", "AQMP URI format")
+	queueName := flag.String("queue", "gollector", "AMQP Queue Name")
+
+	mongoURL := flag.String("mongoUrl", "mongodb://localhost:27017/", "Mongo URL")
+	databaseName := flag.String("database", "gollector", "Database name")
+	collectionName := flag.String("collection", "gollector", "Database Collection name")
+
+	flag.Parse()
+
+	mongoConfig := MongoConfig{*mongoURL, *databaseName, *collectionName}
+	c := mongoConfig.connect()
+
+	consumer := Consumer{*amqpURL, *queueName}
+	consumer.start(c.callbackEvent)
+}
